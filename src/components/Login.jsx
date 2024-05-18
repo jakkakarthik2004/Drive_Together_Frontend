c097192a6348cc06c346cdc5462ae6a2e07d1852
jakkakarthik2004 <118452035+jakkakarthik2004@users.noreply.github.com>
2024-05-18 21:54:47 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,54 +1,50 @@
 import React, { useState } from "react";
 import "./Login.css";
 import axios from "axios";
-import { useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const handleInput = (e) => {
-        if (e.target.name === "email") {
-            setEmail(e.target.value)
+  const handleInput = (e) => {
+    if (e.target.name === "email") {
+      setEmail(e.target.value);
+    } else {
+      setPassword(e.target.value);
+    }
+  };
+
+  const getData = () => {
+    const data = { email, password };
+    axios.post("https://drive-together-backend.onrender.com/login", data)
+      .then(response => {
+        if (response.data.status === false) {
+          alert(response.data.message);
         } else {
-            setPassword(e.target.value)
+          sessionStorage.setItem('username', response.data.user);
+          sessionStorage.setItem('email', response.data.email);
+          sessionStorage.setItem('phone', response.data.phone);
+          localStorage.setItem('token', response.data.token);
+          history.push('/home');
+          alert(response.data.message);
         }
-    }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
-    const getData = () => {
-        const data = { email, password }
-        axios.post("https://drive-together-backend.onrender.com/login", data)
-            .then(response => {
-                if(response.data.status === false) {
-                    alert(response.data.message);
-                }
-                else {
-                    sessionStorage.setItem('username', response.data.user);
-                    sessionStorage.setItem('email', response.data.email);
-                    sessionStorage.setItem('phone', response.data.phone);
-                    localStorage.setItem('token', response.data.token);
-                    history.push('/home')
-                    alert(response.data.message);
-                }
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }
-
-
-    return (
-        <>
-            <div className="login">
-                <h1>Login</h1>
-                <input type="text" placeholder="Email" name="email" onChange={handleInput} autoComplete="off" />
-                <input type="password" placeholder="Password" name="password" onChange={handleInput} autoComplete="off" />
-                <button onClick={getData}>Log in</button>
-            </div>
-        </>
-    );
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <input type="text" placeholder="Email" name="email" onChange={handleInput} autoComplete="off" />
+      <input type="password" placeholder="Password" name="password" onChange={handleInput} autoComplete="off" />
+      <button onClick={getData}>Log in</button>
+    </div>
+  );
 };
 
 export default Login;
